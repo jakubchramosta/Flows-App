e519142854ba7319ecfec6b8d39122cc48611859
jakubchramosta <jakub.chramosta1@gmail.com>
2024-09-20 12:43:12 +0200

    SidebarMenu first try

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,5 +1,21 @@
 function SidebarMenu() {
-  return <h1 className=" bg-stone-900 text-stone-50">Menu</h1>;
+  return (
+    <aside class="bg-gray-800 text-white w-64 min-h-screen p-4">
+      <nav>
+        <ul class="space-y-2">
+          <li class="opcion-con-desplegable">
+            <div class="flex items-center justify-between p-2 hover:bg-gray-700">
+              <div class="flex items-center">
+                <i class="fas fa-calendar-alt mr-2"></i>
+                <span>Main</span>
+              </div>
+              <i class="fas fa-chevron-right mr-2 text-xs"></i>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 }
 
 export default SidebarMenu;
